2b4e1b2a0d6f94c50471909453312057c3554547
tyler weston <tw@a>
2018-11-26 14:55:38 -0600

    fixing double import error

--- nk_postgres/__init__.py
+++ nk_postgres/__init__.py
@@ -1,5 +1,4 @@
 from .psyc import psycopg_cursor, psycopg_query, _psycopg_reset
-from .psyc import psycopg_cursor, psycopg_query, _psycopg_reset
 from .sqla import sqla_cursor, sqla_metadata, sqla_engine
 
 from .util import wait_for_pg_service, validate_db_config
